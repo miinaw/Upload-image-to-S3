df4e55dcc657c4479c501a6f6e0d370317f32c3a
miinaw <yong.abema@gmail.com>
2019-05-18 20:33:19 +0900

    add: description

--- upload-image.py
+++ upload-image.py
@@ -1,7 +1,6 @@
-import json
 import boto3
 
 bucket_name = ""
 s3 = boto3.resource('s3')
 
-s3.Bucket(bucket_name).upload_file('', 'upload.jpg')
+s3.Bucket(bucket_name).upload_file('アップロードするファイルパス', 'バケットのパス')
